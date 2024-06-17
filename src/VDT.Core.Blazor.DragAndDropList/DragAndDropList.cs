753398bec4640377cfa279bc689543058f67c609
Maikel Bos <maikel.bos0@gmail.com>
2024-06-17 17:53:02 +0200

    Add list component with rendertree

---
+++ src/VDT.Core.Blazor.DragAndDropList/DragAndDropList.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System.Collections.Generic;
+
+namespace VDT.Core.Blazor.DragAndDropList;
+
+public class DragAndDropList<TItem> : ComponentBase {
+    [Parameter] public List<TItem> Items { get; set; } = new();
+
+    [Parameter] public RenderFragment<ItemContext<TItem>> ItemTemplate { get; set; } = itemContext => builder => {};
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder) {
+        builder.OpenElement(1, "div");
+        builder.AddAttribute(2, "class", "drag-and-drop-list");
+        builder.OpenRegion(3);
+
+        foreach (var item in Items) {
+            builder.OpenElement(4, "div");
+            builder.AddAttribute(5, "class", "drag-and-drop-list-item");
+            builder.AddContent(6, ItemTemplate(new ItemContext<TItem>(item)));
+            builder.CloseElement();
+        }
+
+        builder.CloseRegion();
+        builder.CloseElement();
+    }
+}
---
+++ src/VDT.Core.Blazor.DragAndDropList/ItemContext.cs
@@ -0,0 +1,9 @@
+﻿namespace VDT.Core.Blazor.DragAndDropList;
+
+public class ItemContext<TItem> {
+    public TItem Item { get; }
+
+    public ItemContext(TItem item) {
+        Item = item;
+    }
+}

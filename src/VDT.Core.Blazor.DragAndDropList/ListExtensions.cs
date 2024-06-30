b0275d12944fb5ee14691556c5c1d20b7ff02180
Maikel Bos <maikel.bos0@gmail.com>
2024-06-30 09:02:23 +0200

    Add list extension method to replace the ReorderedItems property on the event args

---
+++ src/VDT.Core.Blazor.DragAndDropList/ListExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace VDT.Core.Blazor.DragAndDropList;
+
+/// <summary>
+/// Provides static methods for manipulating objects that implement <see cref="IList{T}"/>
+/// </summary>
+public static class ListExtensions {
+    /// <summary>
+    /// Reorders a list according to the given event arguments
+    /// </summary>
+    /// <typeparam name="TItem">Item type</typeparam>
+    /// <param name="source">List to reorder</param>
+    /// <param name="args">Event arguments according to which the list should be reordered</param>
+    public static void Reorder<TItem>(this IList<TItem> source, DropItemEventArgs args) {
+        var item = source[args.OriginalItemIndex];
+        source.RemoveAt(args.OriginalItemIndex);
+        source.Insert(args.NewItemIndex, item);
+    }
+}
